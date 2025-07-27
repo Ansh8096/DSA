422ef87f6a244d6902c8752976e13b968b9b379c
Ansh Verma <anshv8096@gmail.com>
2025-07-27 22:49:47 +0530

    pattern-1.cpp

--- patterns/pattern-1.cpp
+++ patterns/pattern-1.cpp
@@ -1,1 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+/*   * * * *  
+     * * * *  
+     * * * *  
+     * * * *  
+*/
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    // int n;
+    // cin>>n;
+    // print(n);
+
+    int t;
+    cout << "enter how many times you want to print diff. patterns: ";
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        print1(n);
+    }
+
+    return 0;
+}
