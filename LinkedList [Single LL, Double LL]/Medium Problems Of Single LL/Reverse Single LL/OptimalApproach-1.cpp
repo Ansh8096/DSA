e25b9ac193a5a4404fbb87505ed8022c53b5b868
Ansh Verma <anshv8096@gmail.com>
2026-01-03 12:49:10 +0530

    Implement optimal approach to reverse a singly linked list

---
+++ LinkedList [Single LL, Double LL]/Medium Problems Of Single LL/Reverse Single LL/OptimalApproach-1.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+  int data;
+  Node *next;
+
+public:
+  Node(int data1, Node *next1)
+  {
+    data = data1; // Initialize data with the provided value
+    next = next1; // Initialize next with the provided
+  }
+
+public:
+  Node(int data1)
+  {
+    data = data1;   // Initialize data with the provided value
+    next = nullptr; // Initialize next as null since it's the end of the list
+  }
+};
+Node *convrt_arr2LL(vector<int> &a){
+  Node *head = new Node(a[0]); // head node of our LL
+  Node *mover = head;          // starting mover ptr from head node ptr
+  for (int i = 1; i < a.size(); i++)
+  {
+    Node *temp = new Node(a[i]); // creating temp. node for every a[i]..
+    mover->next = temp;          // updating the mover ptr
+    // implement mover_ptr for creating a LL...
+    mover = temp; // OR (mover = mover->next)
+  }
+  // returning the head Node of our LL...
+  return head;
+}
+void print(Node *head){
+  while (head)
+  {
+    cout << head->data << " ";
+    head = head->next;
+  }
+}
+Node* reverse_SLL_OP1(Node *head)
+{
+  Node *temp = head;
+  Node *prev = NULL;
+  Node *front = NULL;
+  while (temp != NULL)
+  {
+    // 'front' to preserve the reference
+    front = temp->next;
+    // Reverse the direction of the
+    // current node's 'next' pointer
+    // to point to 'prev'
+    temp->next = prev;
+    // Move 'prev' to the current
+    // node for the next iteration
+    prev = temp;
+    // Move 'temp' to the 'front' node
+    // advancing the traversal
+    temp = front;
+  }
+  // Return the new head of
+  // the reversed linked list
+  return prev;
+}
+int main(){
+  vector<int> a = {0,1,2,100,1,0,2};
+  Node *head = convrt_arr2LL(a);
+  head = reverse_SLL_OP1(head);
+  print(head);
+  return 0;                                                                                          
+}
