a571335f1e77718c9de2f62c0bfdc4afa0ee61be
Ansh Verma <anshv8096@gmail.com>
2026-01-06 23:55:14 +0530

    Add brute force approach for loop detection in LL
    Implemented a brute force approach to find the starting node of a loop in a linked list using a hashmap.

---
+++ LinkedList [Single LL, Double LL]/Medium Problems Of Single LL/Find the starting point of loop in LL/BruteForceApproach.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+  int data;
+  Node *next;
+
+public:
+  Node(int data1, Node *next1)
+  {
+    data = data1; // Initialize data with the provided value
+    next = next1; // Initialize next with the provided
+  }
+
+public:
+  Node(int data1)
+  {
+    data = data1;   // Initialize data with the provided value
+    next = nullptr; // Initialize next as null since it's the end of the list
+  }
+};
+Node* starting_node_of_LL_BR(Node* head){
+  Node* temp = head;
+  map<Node*,int> mpp;
+  while(temp != NULL){
+    if (mpp.find(temp) != mpp.end()){
+      return temp; // it is the starting node of the cycle in LL..
+    }
+    mpp[temp] = 1;
+    temp = temp->next;
+  }
+  return NULL; // when there is no loop present in LL....
+}
+int main(){
+
+  // we need to create each node of Linked list seperately to create a cycle...
+  Node* head = new Node(1);
+  Node* second = new Node(2);
+  Node* third = new Node(3);
+  Node* fourth = new Node(4);
+  Node* fifth = new Node(13);
+  Node* sixth = new Node(6);
+  Node* seventh = new Node(7);
+  Node* eight = new Node(8);
+  Node* ninth = new Node(9);
+    
+  // linking these nodes...
+  head->next = second;
+  second->next = third;
+  third->next = fourth;
+  fourth->next = fifth;
+  fifth->next = sixth;
+  sixth->next = seventh;
+  seventh->next = eight;
+  eight->next = ninth;
+    
+  // creates a loop:
+  ninth->next = third; 
+
+  Node* ans = starting_node_of_LL_BR(head);
+  cout<<ans->data;
+  return 0;                                                                                          
+}
