8ff5364e48f19f3aa799608a47f60dfff573b65c
Ansh Verma <anshv8096@gmail.com>
2026-03-15 00:00:26 +0530

    Implement bit flipping calculation in Approach2.cpp

---
+++ Bit Manipulation/Interview Problems/Min. no. of bits to flip/Approach2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minimumNumberOfBitsToFlip_2(int start,int goal){
+    int num = start ^ goal;
+    int cnt = 0;
+    while (num >= 1)
+    {
+        // 'num & 1' checks for odd no...
+        cnt += (num & 1); // counting the set bits only..
+        num = num >> 1; // trimming the num...
+    }
+    return cnt;
+}
+int main(){
+    cout<<minimumNumberOfBitsToFlip_2(4,5);
+    return 0;
+}
