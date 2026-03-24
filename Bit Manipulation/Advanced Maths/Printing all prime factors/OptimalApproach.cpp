2ec63b8077ca48fab31ef4184dfd48a21ba86d32
Ansh Verma <anshv8096@gmail.com>
2026-03-24 23:45:40 +0530

    Create OptimalApproach.cpp

---
+++ Bit Manipulation/Advanced Maths/Printing all prime factors/OptimalApproach.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> printingAllPrimeFactors_OP(int n){
+    vector<int> ans;
+    for (int i = 2; i*i <= n ; i++)
+    {
+        if(n%i == 0){
+            ans.push_back(i);
+            while(n%i == 0) n = n/i;
+        }
+    }
+    if(n!=1) ans.push_back(n);
+    return ans;
+}
+int main(){
+    
+    int n;
+    cout<<"enter the value of n :"<<endl;
+    cin>>n;
+
+    vector<int> ans = printingAllPrimeFactors_OP(n);
+    for (auto itt : ans) cout<<itt<<" ";
+    
+    return 0;
+}
