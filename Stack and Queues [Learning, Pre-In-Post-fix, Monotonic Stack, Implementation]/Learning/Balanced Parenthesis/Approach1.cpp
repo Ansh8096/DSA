ad1c71f62d8b191f4674d070d3af37aa2b6d99de
Ansh Verma <anshv8096@gmail.com>
2026-03-29 23:46:52 +0530

    Implement balanced parenthesis checker in Approach1.cpp

---
+++ Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Learning/Balanced Parenthesis/Approach1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool BalanceAndValidParenthesis(string s)
+{
+    stack<char> stk;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+            stk.push(s[i]);
+        }
+
+        else
+        {
+            // means there is no opening parenthesis present in stack for closing parenthesis...
+            if (stk.size() == 0) return false;
+
+            char poped = stk.top();
+            stk.pop();
+            if ((s[i] == ')' && poped == '(') or (s[i] == ']' && poped == '[') or (s[i] == '}' && poped == '{'))
+            {
+                continue;
+            }
+            else return false;
+        }
+    }
+    return stk.empty();
+}
+int main(){
+    
+    cout<< BalanceAndValidParenthesis("(){[}]");
+
+    return 0;
+}
