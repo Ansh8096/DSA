fe0ee97d02f15cd93f066b48a578dc6ed0cb0f59
Ansh Verma <anshv8096@gmail.com>
2026-04-01 23:41:44 +0530

    Implement postfix to infix conversion in approach1.cpp

---
+++ Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Conversion Problems/Postfix To Infix/approach1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+string postfixToInfix(string s){
+    stack<string> stk;
+    int i = 0;
+    while (i < s.length())
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z' or
+           s[i] >= 'a' && s[i] <= 'z' or
+           s[i] >= '0' && s[i] <= '9' ){
+            stk.push(string(1, s[i]));
+        }
+
+        else{
+            string top1 = stk.top();
+            stk.pop();
+            string top2 = stk.top();
+            stk.pop();
+            // This string generation takes t.c of (o(n1) + o(n2)) in some languages...
+                // where: n1 = length of first string , n2 = length of second string    
+            string reqString = '(' + top2 + s[i] + top1 + ')'; 
+
+            stk.push(reqString);
+        }
+        i++;
+    }
+    return stk.top();
+}
+int main(){
+
+    cout<<postfixToInfix("AB-DE+F*/");
+
+    return 0;
+}
