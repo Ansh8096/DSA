3c1a6c134bbc3b5fe4f1859122da651b253d830f
Ansh Verma <anshv8096@gmail.com>
2026-04-02 23:54:26 +0530

    Implement prefix to infix conversion in Approach1.cpp

---
+++ Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Conversion Problems/Prefix To Infix/Approach1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+string prefixToInfix(string s){
+    stack<string> stk;
+    int i = s.length()-1;
+    while (i >= 0){
+        if(s[i] >= 'A' && s[i] <= 'Z' or
+           s[i] >= 'a' && s[i] <= 'z' or
+           s[i] >= '0' && s[i] <= '9' ){
+            stk.push(string(1, s[i]));
+        }
+
+        else{
+            string top1 = stk.top();
+            stk.pop();
+            string top2 = stk.top();
+            stk.pop();
+            // This string generation takes t.c of (o(n1) + o(n2)) in some languages...
+            // where: n1 = length of first string , n2 = length of second string    
+            string reqString = '(' + top1 + s[i] + top2 + ')'; 
+
+            stk.push(reqString);
+        }
+        i--;
+    }
+    return stk.top();
+}
+int main(){
+
+    cout<<prefixToInfix("*+PQ-MN");
+
+    return 0;
+}
