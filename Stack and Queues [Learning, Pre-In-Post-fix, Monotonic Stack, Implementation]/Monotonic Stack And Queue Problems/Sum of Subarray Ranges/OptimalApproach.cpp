a92ca9455164bc997c7a63138df941dc1366f737
Ansh Verma <anshv8096@gmail.com>
2026-04-06 23:37:23 +0530

    Implement optimal approach for subarray range sums

---
+++ Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Monotonic Stack And Queue Problems/Sum of Subarray Ranges/OptimalApproach.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> findNSE(vector<int>&a,int n){
+    vector<int> nse(n);
+    stack<int>stk;
+    for (int i = n-1; i >= 0; i--)
+    {
+        while (!stk.empty() and a[stk.top()] >= a[i]) stk.pop();
+        nse[i] = (stk.empty()) ? n : stk.top();
+        stk.push(i);
+    }
+    return nse;
+    
+}
+vector<int> findPSEE(vector<int>&a,int n){
+    vector<int> psee(n);
+    stack<int>stk;
+    for (int i = 0; i < n; i++)
+    {
+        while (!stk.empty() and a[stk.top()] > a[i]) stk.pop();
+        psee[i] = (stk.empty()) ? -1 : stk.top();
+        stk.push(i);
+    }
+    return psee;
+    
+}
+int sumOfMinOfSubArr_OP(vector<int>& a,int n){
+    vector<int> nse = findNSE(a,n);
+    vector<int> psee = findPSEE(a,n);
+
+    int total = 0;
+    int mod = (int)(1e9+7);
+    for (int i = 0; i < n; i++)
+    {
+        int left = i-psee[i];
+        int right = nse[i] - i;
+        total = (total+(right*left*1LL*a[i])%mod)%mod;
+    }
+    return total;
+}
+vector<int> findNGE(vector<int>&a,int n){
+    vector<int> NGE(n);
+    stack<int>stk;
+    for (int i = n-1; i >= 0; i--)
+    {
+        while (!stk.empty() and a[stk.top()] <= a[i]) stk.pop();
+        NGE[i] = (stk.empty()) ? n : stk.top();
+        stk.push(i);
+    }
+    return NGE;
+}
+vector<int> findPGEE(vector<int>&a,int n){
+    vector<int> PGEE(n);
+    stack<int>stk;
+    for (int i = 0; i < n; i++){
+        while (!stk.empty() and a[stk.top()] < a[i]) stk.pop();
+        PGEE[i] = (stk.empty()) ? -1 : stk.top();
+        stk.push(i);
+    }
+    return PGEE;
+}
+int sumOfMaxOfSubArr_OP(vector<int>& a,int n){
+    vector<int> NGE = findNGE(a,n);
+    vector<int> PGEE = findPGEE(a,n);
+
+    int total = 0;
+    int mod = (int)(1e9+7);
+    for (int i = 0; i < n; i++)
+    {
+        int left = i - PGEE[i];
+        int right = NGE[i] - i;
+        total = (total+(right*left*1LL*a[i])%mod)%mod;
+    }
+    return total;
+}
+int SumOfSubmissionOfRange_OP(vector<int> a,int n){
+    return sumOfMaxOfSubArr_OP(a,n) - sumOfMinOfSubArr_OP(a,n);
+}
+int main(){
+
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin >> n;
+
+    vector<int> a(n);
+    cout<<"Enter the values of the array: "<<endl;
+    for(int i = 0; i<n ;i++) cin>>a[i];
+    cout<<SumOfSubmissionOfRange_OP(a,n);
+
+    return 0;
+}
