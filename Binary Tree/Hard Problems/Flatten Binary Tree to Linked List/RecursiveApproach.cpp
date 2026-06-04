1c81ad09a52393143e6d245af41727a2844e8635
Ansh Verma <anshv8096@gmail.com>
2026-06-04 23:57:42 +0530

    Add recursive approach to flatten binary tree

---
+++ Binary Tree/Hard Problems/Flatten Binary Tree to Linked List/RecursiveApproach.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    
+    TreeNode(int val1){
+        val = val1;
+        left = NULL;
+        right = NULL;
+    }
+};
+void printTree(TreeNode* root, int space = 0, int gap = 5){
+
+    if(root == NULL) return;
+
+    // Increase distance between levels
+    space += gap;
+
+    // Print right subtree first
+    printTree(root->right, space);
+
+    // Print current node
+    cout << endl;
+
+    for(int i = gap; i < space; i++){
+        cout << " ";
+    }
+
+    cout << root->val << "\n";
+
+    // Print left subtree
+    printTree(root->left, space);
+}
+void traverseBt(TreeNode* node, TreeNode* &prev){
+    if(node == NULL) return;
+    traverseBt(node->right,prev);
+    traverseBt(node->left,prev);
+    node->right = prev;
+    node->left = NULL;
+    prev = node;
+}
+void flattenToLL_rec(TreeNode* root) { 
+    TreeNode* prev = NULL;
+    traverseBt(root,prev);
+}
+int main(){
+    
+    // Constructing tree:
+    //
+    //         3
+    //        / \
+    //       9   20
+    //          /  \
+    //         15   7
+
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    
+    cout << "Original Tree:\n";
+    printTree(root);
+
+    flattenToLL_rec(root);
+
+    cout << "\n\nFlattened Linked List:\n";
+
+    TreeNode* curr = root;
+
+    while(curr){
+        cout << curr->val;
+
+        if(curr->right){
+            cout << " -> ";
+        }
+
+        curr = curr->right;
+    }
+
+    cout << endl;
+  
+    return 0;
+}
