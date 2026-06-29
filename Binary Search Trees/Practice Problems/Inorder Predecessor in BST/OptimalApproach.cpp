bc7695e8f6a8d0463106c1ae8607e29ca935efc4
Ansh Verma <anshv8096@gmail.com>
2026-06-29 21:09:10 +0530

    Add optimal approach for inorder predecessor in BST
    Implement an optimal approach to find the inorder predecessor of a given node in a Binary Search Tree (BST). The code includes a TreeNode class, a function to print the tree, and the main function to demonstrate the predecessor finding.

---
+++ Binary Search Trees/Practice Problems/Inorder Predecessor in BST/OptimalApproach.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TreeNode{
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    
+    TreeNode(int val1){
+        val = val1;
+        left = NULL;
+        right = NULL;
+    }
+};
+void printTree(TreeNode* root, int space = 0, int gap = 5){
+
+    if(root == NULL) return;
+
+    // Increase distance between levels
+    space += gap;
+
+    // Print right subtree first
+    printTree(root->right, space);
+
+    // Print current node
+    cout << endl;
+
+    for(int i = gap; i < space; i++){
+        cout << " ";
+    }
+
+    cout << root->val << "\n";
+
+    // Print left subtree
+    printTree(root->left, space);
+}
+int InorderPredecessor_OP(TreeNode* root, TreeNode* x){
+    TreeNode* curr = root;
+    int ans = INT_MIN;
+    while (curr)
+    {
+        if(curr->val >= x->val) curr = curr->left;
+        else{
+            ans = max(ans,curr->val);
+            curr = curr->right;
+        }
+    }
+        
+    return (ans == INT_MIN) ? -1 : ans;
+}
+int main(){
+
+    // Constructing BST:
+    //
+    //          8
+    //        /   \
+    //       3     10
+    //      / \      \
+    //     1   6      14
+    //        / \     /
+    //       4   7   13
+
+    TreeNode* root = new TreeNode(8);
+
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(10);
+
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(6);
+
+    root->left->right->left = new TreeNode(4);
+    root->left->right->right = new TreeNode(7);
+
+    root->right->right = new TreeNode(14);
+    root->right->right->left = new TreeNode(13);
+
+    cout << "Binary Search Tree:\n";
+    printTree(root);
+
+    // Find inorder successor of node 6
+    TreeNode* x = root->left->right;   // Node with value 6
+
+    int predecessor = InorderPredecessor_OP(root, x);
+
+    cout << "\n\nNode: " << x->val << endl;
+
+    if(predecessor != -1)
+        cout << "Inorder predecessor: " << predecessor << endl;
+    else
+        cout << "Inorder predecessor does not exist.\n";
+
+    return 0;
+}
